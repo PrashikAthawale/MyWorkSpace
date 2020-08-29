cd24202a7cc21cc6cf2f0613e9d8c9db80829eef
Prashik Athawale <57024961+PrashikAthawale@users.noreply.github.com>
2020-08-29 14:48:25 +0530

    Add files via upload

---
+++ C Language/C Programs/Problems on Numbers/Assignment3/Assignment3_4/helper.c
@@ -0,0 +1,26 @@
+#include"header.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+//Function Name:-DisplayEven
+//Input:-Integer
+//Output:-Void
+//Description:- It is used to which accept one number from user 
+//and print that number of even number on Screen
+//Author:-Prashik Athawale
+//Date:-31 July 2020
+//////////////////////////////////////////////////////////////////////////////////
+
+void DisplayConvert(char cNo)
+{
+		char ch='\0';
+		
+		
+		
+		ch=cNo+32;
+	
+	printf("%c",ch);
+	
+	
+	
+}
+
