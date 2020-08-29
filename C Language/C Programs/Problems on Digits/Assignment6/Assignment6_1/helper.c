504f417424c80cd0de65678caac6731767453f84
Prashik Athawale <57024961+PrashikAthawale@users.noreply.github.com>
2020-08-29 15:04:40 +0530

    Add files via upload

---
+++ C Language/C Programs/Problems on Digits/Assignment6/Assignment6_1/helper.c
@@ -0,0 +1,23 @@
+#include"header.h"
+
+//////////////////////////////////////////////////////////////////////////////////
+//Function Name:-RevNum
+//Input:-Integer
+//Output:-Void
+//Description:-Write a program which accept number from user and display its digits in reverse order.
+//Author:-Prashik Athawale
+//Date:-6 August 2020
+//////////////////////////////////////////////////////////////////////////////////
+
+void RevNum(int iNo)
+{	
+	 int iDigit=0;
+	 
+	 while(iNo!=0){
+		 
+		 iDigit=iNo%10;
+		 printf("%d\n",iDigit);
+		 
+		 iNo=iNo/10;
+	 }
+}
