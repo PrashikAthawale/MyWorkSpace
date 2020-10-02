051892d5c405470f3dcd3f2be9b56dae4146d925
Prashik Athawale <57024961+PrashikAthawale@users.noreply.github.com>
2020-10-02 14:03:58 +0530

    Add files via upload

---
+++ Assignment58/Assignment58_3/Demo.java
@@ -0,0 +1,59 @@
+/*
+//////////////////////////////////////////////////////////////////////////////////
+//Function Name:-Display
+//Input:-Integer  
+//Output:-void
+//Author:-Prashik Athawale
+//Date:-1 October 2020
+//////////////////////////////////////////////////////////////////////////////////
+
+Problem Statement:-Write Java program which accept number of rows and number of 
+columns from user and display below pattern. 
+
+Input : iRow = 3 iCol = 5 
+Output : 5 4 3 2 1 
+         5 4 3 2 1 
+         5 4 3 2 1
+*/
+
+import java.lang.*;
+import java.util.*;
+
+class Pattern
+{
+	void Display(int iRow,int iCol)
+	{
+		
+		for(int i=1;i<=iRow;i++)
+		{
+			for(int j=iCol;j>=1;j--)
+			{
+				System.out.print(j+"\t");
+				
+			}
+			  System.out.println();
+			
+		}
+		
+	}
+	
+}
+
+class Demo
+{
+	public static void main(String arg[])
+	{
+		Scanner sobj=new Scanner(System.in);
+		
+		System.out.print("Enter Number of Rows :");
+		int iRow=sobj.nextInt();
+		
+		System.out.print("Enter Number of Columns :");
+		int iCol=sobj.nextInt();
+		
+		Pattern obj=new Pattern();
+		
+		obj.Display(iRow,iCol);
+			
+	}
+}
